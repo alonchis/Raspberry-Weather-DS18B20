5a937af31c154f6b59325f4dceb4560567a2d7f3
alonchis <alonso.yo2@gmail.com>
2019-02-17 17:09:40 -0600

    commented out some unnecessary enum

--- getInfo.py
+++ getInfo.py
@@ -19,9 +19,9 @@ device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
 es = elasticsearch.Elasticsearch(['alonsoarteaga.com:9200'])
-sensor_args = { '11': Adafruit_DHT.DHT11,
-                '22': Adafruit_DHT.DHT22,
-                '2302': Adafruit_DHT.AM2302 }
+#sensor_args = { '11': Adafruit_DHT.DHT11,
+#                '22': Adafruit_DHT.DHT22,
+#                '2302': Adafruit_DHT.AM2302 }
 sensor = 22
 pin = 4
 
@@ -38,7 +38,7 @@ temperature = temperature * 9.0  / 5.0 + 32
 # If this happens try again!
 def get_readings_dht22():
   if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+      print('DHT22 readings: Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
     result = "humidity = {}, temperature = {}".format(humidity, temperature)
     return result
   else:
